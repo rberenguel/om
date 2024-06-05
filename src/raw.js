acef274a7bfbb0b766b46e51f1b496f45016ca93
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2024-06-05 20:34:34 +0200

    Improve some things

--- src/raw.js
+++ src/raw.js
@@ -4,21 +4,17 @@ import weave from "../src/weave.js";
 
 import { parseIntoWrapper, toMarkdown } from "./parser.js";
 import { wireEverything } from "./load.js";
-import { manipulation } from "./manipulation.js";
 
 const unrawPane = (body, container) => {
   parseIntoWrapper(body.innerText, body);
   container.raw = false;
   wireEverything(weave.buttons(weave.root));
-  container.style.minHeight = "";
 };
 
 const rawPane = (body, container) => {
   const md = toMarkdown(container.querySelector(".body"));
   body.innerText = md;
   container.raw = true;
-  container.style.minHeight = "fit-content";
-  container.style.height = "";
 };
 
 const raw = {
